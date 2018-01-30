9927231127831486ac8358cef49ef020e1e7c46f
WangGeWu <35912578+WangGeWu@users.noreply.github.com>
2018-01-30 11:20:34 +0800

    Add files via upload

---
+++ 甲级/1037.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Ì°ÐÄËã·¨
+int cmp(const void *a, const void *b)
+{
+	return (*(int*)a) - (*(int*)b);
+}
+int main()
+{
+	int n, m;
+	int i;
+	//input
+	scanf("%d", &n);
+	int *coupon = (int*)malloc(n*sizeof(int));
+	for (i = 0; i < n; i++)
+		scanf("%d", &coupon[i]);
+	scanf("%d", &m);
+	int *product = (int*)malloc(m * sizeof(int));
+	for (i = 0; i < m; i++)
+		scanf("%d", &product[i]);
+	//process
+	qsort(coupon, n, sizeof(int), cmp);
+	qsort(product, m, sizeof(int), cmp);
+	i = 0;
+	int value=0;
+	while (coupon[i] < 0 && product[i] < 0)
+	{
+		value += coupon[i] * product[i];
+		i++;
+	}
+	int j = m - 1;
+	i = n - 1;
+	while (coupon[i] > 0 && product[j] > 0)
+	{
+		value += coupon[i] * product[j];
+		i--; j--;
+	}
+	printf("%d\n", value);
+}
