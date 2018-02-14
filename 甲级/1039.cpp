2aab184062b6ef187edf470e9bce1c8088fe718b
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-02-14 19:59:09 +0800

    Update 1039.cpp

--- 甲级/1039.cpp
+++ 甲级/1039.cpp
@@ -1,85 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-struct classnode
-{
-	char stu_name[40000][5];
-	int stu_num;
-	int id;//Ë÷Òý
-};
-typedef struct classnode* Pcourse;
 struct stunode
 {
-	char name[5];
 	int find_id[50];
-	int num;//¿ÆÄ¿Êý
-};
-typedef struct stunode* Pstu;
+	int num;//ç§‘ç›®æ•°
+}stu[175850];
 int cmp(const void *a, const void *b)
 {
 	return *(int*)a - *(int*)b;
 }
 int main()
 {
-	int i, j,k;
+	int i, j, k;
 	int n, m;
 	//input
 	scanf("%d %d", &n, &m);
-	Pcourse course = (Pcourse)malloc(sizeof(struct classnode)*m);
-	Pstu stu = (Pstu)malloc(sizeof(struct stunode)*n);
 	for (i = 0; i < m; i++)
 	{
 		int findnum, stunum;
 		scanf("%d %d", &findnum, &stunum);
-		course[i].id = findnum;
-		course[i].stu_num = stunum;
-		/*
-		 *²ÉÓÃmalloc·ÖÅäÄÚ´æ
-		 *course[i].stu_name = (char**)malloc(sizeof(char*)*stunum);
-		 *for(k=0;k<stunum;k++)
-		 *    (course[i].stu_name[k]) = (char*)malloc(sizeof(char) * 5);
-		 */
 		for (j = 0; j < stunum; j++)
 		{
-			getchar();
-			scanf("%s", course[i].stu_name[j]);
+			char id[5];
+			scanf("%s",id);
+			int v = ((id[0] - 'A') * 26 * 26 + (id[1] - 'A') * 26 + id[2] - 'A')*10 + id[3] - '0';
+			stu[v].find_id[stu[v].num++] = findnum;
 		}
 	}
-	for (i = 0; i < n; i++)
-	{
-		getchar();
-		scanf("%s", stu[i].name);
-	}
-	//process
 	//find
-	int cnt;
 	for (i = 0; i < n; i++)
 	{
-		cnt = 0;
-		for (j = 0; j < m; j++)
+		char s[5];
+		scanf("%s", s);
+		int v = ((s[0] - 'A') * 26 * 26 + (s[1] - 'A') * 26 + s[2] - 'A')*10 + s[3] - '0';
+		qsort(stu[v].find_id,stu[v].num,sizeof(int),cmp);
+		printf("%s %d", s, stu[v].num);
+		for (j = 0; j < stu[v].num; j++)
 		{
-			for (k = 0; k <course[j].stu_num ; k++)
-			{
-				if (strcmp(stu[i].name, course[j].stu_name[k])==0)//ÕÒµ½
-				{
-					stu[i].find_id[cnt++] = course[j].id;
-				}
-			}
-		}
-		stu[i].num = cnt;
-	}
-	//output
-	for (i = 0; i < n; i++)
-	{
-		qsort(stu[i].find_id, stu[i].num, sizeof(int), cmp);
-		printf("%s %d", stu[i].name, stu[i].num);
-		for (j = 0; j < stu[i].num; j++)
-		{
-			printf(" %d", stu[i].find_id[j]);	
+			printf(" %d", stu[v].find_id[j]);
 		}
 		printf("\n");
 	}
-	free(stu);
-	free(course);
+	//output
 	return 0;
 }
