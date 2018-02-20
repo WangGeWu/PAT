06d7f33c775b7a196515253884da7a953f943fd0
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-02-20 19:25:22 +0800

    Rename 1047.cpp to 1047.c

--- 甲级/1047.c
+++ 甲级/1047.c
@@ -36,7 +36,7 @@ int main()
 	qsort(stu, n, sizeof(struct stunode), cmp);
 	for (i = 0; i < m; i++)
 		course[i].course_num = 0;
-	//遍历排好序的学生结构体数组，将每个学生选择的课程记录到课程结构体数组中，课程数组的下标代表这个节点存放的课程号-1
+	//閬嶅巻鎺掑ソ搴忕殑瀛︾敓缁撴瀯浣撴暟缁勶紝灏嗘瘡涓鐢熼�夋嫨鐨勮绋嬭褰曞埌璇剧▼缁撴瀯浣撴暟缁勪腑锛岃绋嬫暟缁勭殑涓嬫爣浠ｈ〃杩欎釜鑺傜偣瀛樻斁鐨勮绋嬪彿-1
 	for (j = 0; j < n; j++)
 	{
 		for (int k = 0; k < stu[j].stu_num; k++)
