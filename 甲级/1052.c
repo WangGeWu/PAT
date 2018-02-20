ee0a8f62645b4e326d754938f1282701c65eb9f1
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-02-20 20:03:49 +0800

    Rename 1052.cpp to 1052.c

--- 甲级/1052.c
+++ 甲级/1052.c
@@ -8,7 +8,7 @@ struct node {
 }list[100001];
 int cmp(const void *a, const void *b)
 {
-	if (((struct node*)a)->flag != ((struct node*)b)->flag)//flag==1µÄÔÚflag==0µÄÇ°Ãæ
+	if (((struct node*)a)->flag != ((struct node*)b)->flag)//flag==1çš„åœ¨flag==0çš„å‰é¢
 		return ((struct node*)b)->flag - ((struct node*)a)->flag;
 	else
 		return ((struct node*)a)->num - ((struct node*)b)->num;
@@ -28,7 +28,7 @@ int main()
 		list[a].next = c;
 	}	
 	int cnt = 0;
-	for (i=head;i != -1; i = list[i].next)//±éÀúÁ´±í£¬·ÃÎÊ¹ıµÄ½«flag±ê¼ÇÎª1
+	for (i=head;i != -1; i = list[i].next)//éå†é“¾è¡¨ï¼Œè®¿é—®è¿‡çš„å°†flagæ ‡è®°ä¸º1
 	{
 		list[i].flag = 1;
 		cnt++;
@@ -38,7 +38,7 @@ int main()
 		printf("0 -1");
 		return 0;
 	}
-	qsort(list, 100001, sizeof(struct node), cmp);//ÅÅĞò£¬½«·ÃÎÊ¹ıµÄÇÒÖµĞ¡µÄÅÅÔÚÇ°Ãæ£¬
+	qsort(list, 100001, sizeof(struct node), cmp);//æ’åºï¼Œå°†è®¿é—®è¿‡çš„ä¸”å€¼å°çš„æ’åœ¨å‰é¢ï¼Œ
 	printf("%d %05d\n", cnt, list[0].address);
 	for (i = 0; i < cnt; i++)
 	{
