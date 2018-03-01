de690ec9151f08f5cae0c3375108968ef8bf9315
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-03-01 21:26:55 +0800

    Update 1106.c

--- 甲级/1106.c
+++ 甲级/1106.c
@@ -1,28 +1,30 @@
+//因为不知道每个节点的最多有多少子节点，所以在结构体内开辟孩子节点的数组时，很难把握开辟的大小，所以用了C++里面的vector
 #include<stdio.h>
 #include<math.h>
+#include<vector>
+using namespace std;
 struct node
 {
 	int deep;
-	int child[160];
-	int childnum;
+	vector<int>child;
 }tree[100005];
-double min =999999999.9;
+double Min = 999999999.9;
 double price, p, r;
-int num=0;
+int num = 0;
 void dfs(int n)
 {
-	if (tree[n].childnum == 0)
+	if (tree[n].child.size()== 0)
 	{
-		price = p*pow(1+r, tree[n].deep);
-		if (price < min)
+		price = p*pow(1 + r, tree[n].deep);
+		if (price < Min)
 		{
-			min = price;
+			Min = price;
 			num = 1;
 		}
-		else if (price == min)
+		else if (price == Min)
 			num++;
 	}
-	for (int i = 0; i < tree[n].childnum; i++)
+	for (int i = 0; i < tree[n].child.size(); i++)
 	{
 		tree[tree[n].child[i]].deep = tree[n].deep + 1;
 		dfs(tree[n].child[i]);
@@ -38,11 +40,14 @@ int main()
 	{
 		int m;
 		scanf("%d", &m);
-		tree[i].childnum = m;
 		for (j = 0; j < m; j++)
-			scanf("%d", &tree[i].child[j]);
+		{
+			int node;
+			scanf("%d", &node);
+			tree[i].child.push_back(node);
+		}
 	}
 	dfs(0);
-	printf("%.4lf %d", min, num);
+	printf("%.4lf %d", Min, num);
 	return 0;
 }
